a07608152b1eec80bbf9495d6a892297b616a5c5
ezequiel <quielneres@gmail.com>
2020-04-11 00:08:01 -0300

    cadastro de clientes

--- src/pages/singUp/index.js
+++ src/pages/singUp/index.js
@@ -1,46 +1,28 @@
-import React from 'react';
-import {Text, TouchableOpacity, StyleSheet, View} from 'react-native';
+import React, {useState} from 'react';
+import {Text, StyleSheet, View, TextInput} from 'react-native';
+import {Hoshi} from 'react-native-textinput-effects';
 
-import {Container, Card, Content, Form, Item, Input, Label} from 'native-base';
 
 const SingUp = ({navigation}) => {
+
+    const [nameUser, setName] = useState('');
     return (
-        <Container>
-            <Content style={styles.content}>
-                <Card style={styles.card} transparent>
-                    <Form style={styles.form}>
-                        <View style={styles.item}>
-                            <Label style={styles.label}>Nome</Label>
-                            <Input style={styles.input}/>
-                        </View>
-                        <View style={styles.item}>
-                            <Label style={styles.label}>CPF</Label>
-                            <Input style={styles.input}/>
-                        </View>
-                        <View style={styles.item}>
-                            <Label style={styles.label}>E-mail</Label>
-                            <Input style={styles.input}/>
-                        </View>
-                        <View style={styles.item}>
-                            <Label style={styles.label}>Telefone</Label>
-                            <Input style={styles.input}/>
-                        </View>
-                        <View style={styles.item}>
-                            <Label style={styles.label}>Cnpj</Label>
-                            <Input style={styles.input}
-                                placeholder={'Cpj'}
-                            />
-                        </View>
-                    </Form>
-                </Card>
-            </Content>
-        </Container>
+        <View style={styles.content}>
+            <Text>Register</Text>
+            <Hoshi
+                value={nameUser}
+                onChangeText={name => setName(name)}
+            />
+        </View>
     )
+
+
 }
 
 const styles = StyleSheet.create({
     content: {
         // backgroundColor: '#4CB1F7'
+        flex: 1
     },
     card: {
         padding: 15,
@@ -74,6 +56,5 @@ const styles = StyleSheet.create({
     }
 });
 
+
 export default SingUp;
-
-
